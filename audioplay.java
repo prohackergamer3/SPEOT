addaab86f96d44c652793f2f8672b4a98f2bb41a
ZEUX GAMER <109979523+prohackergamer3@users.noreply.github.com>
2023-08-23 17:57:30 +0300

    main

--- audioplay.java
+++ audioplay.java
@@ -2,11 +2,7 @@ import javax.sound.sampled.*;
 import java.io.File;
 public class audioplay {
 	public static void main(String[] args) {
-		File f = new File("output.mp3");
-		AudioInputStream ai = new AudioSystem.getAudioInputStream(f);
-		Clip clip = AudioSystem.getClip();
-		clip.open(ai);
-		clip.start();
+		System.out.println("Dosyanız hazır!");
 		
 	}
 }
