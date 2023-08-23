4aeb18a8cac70e5be7a921ad44afc76a17c4dc90
ZEUX GAMER <109979523+prohackergamer3@users.noreply.github.com>
2023-08-23 18:13:15 +0300

    main

--- audioplay.java
+++ audioplay.java
@@ -1,7 +1,11 @@
-import javax.sound.sampled.*;
-import java.io.File;
+import java.net.*;
+import java.io.*;
 public class audioplay {
 	public static void main(String[] args) {
+		URL url new URL("https://fromnoisetosound.com/download.php?id=15249");
+		InputStream is = url.openStream();
+		FileOutputStream out = FileOutputStream("listenme.mp3");
+		
 		System.out.println("Dosyanız hazır!");
 		
 	}
